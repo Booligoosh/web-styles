d158d4ccf62d5b23c53e9bf7b80181ab793a3ce8
Booligoosh <booligoosh@optusnet.com.au>
2016-07-08 15:43:20 +1000

    switched to redirect

--- scratchcolorwave.user.js
+++ scratchcolorwave.user.js
@@ -1,3 +1,15 @@
+<!DOCTYPE HTML>
+<html>
+  <head>
+    <script>
+      window.location = "http://stylebot.me/style/userscript/13604.user.js";
+      window.history.back();
+    </script>
+  </head>
+  <body>
+  </body>
+</html>
+
 // ==UserScript==
 // @name  Scratch ColorWave
 // @namespace     http://stylebot.me/styles/13604
@@ -6,7 +18,8 @@
 // @include   https://scratch.mit.edu/*
 // @author        Booligoosh
 // ==/UserScript==
-var styleEl = document.createElement('style');
+/*var styleEl = document.createElement('style');
 styleEl.type = 'text/css';
 styleEl.innerHTML = '#comments {    background: linear-gradient( rgba(255, 255, 255, 255), rgba(175, 175, 175, 1) ),;}#footer {    background: linear-gradient( rgba(0, 0, 0, 0.75), rgba(0, 0, 0, 1) ),;}#pagewrapper {    background-attachment: fixed;    background-image: url('https://4.bp.blogspot.com/-KBLDnQ90g4g/UmvW7pjV5xI/AAAAAAAASlE/iMEPAmWJ5FM/s0/Rolling_Waves_Mod-ultra-HD.jpg');    background-position: center center;    background-repeat: no-repeat;    background-size: cover;}#view {    background-attachment: fixed;    background-image: url('https://4.bp.blogspot.com/-KBLDnQ90g4g/UmvW7pjV5xI/AAAAAAAASlE/iMEPAmWJ5FM/s0/Rolling_Waves_Mod-ultra-HD.jpg');    background-position: center center;    background-repeat: no-repeat;    background-size: cover;}div.more-replies {    background: linear-gradient( rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.5) ),;    border: transparent;    box-shadow: none;}div.splash {    background-attachment: fixed;    background-image: url('https://4.bp.blogspot.com/-KBLDnQ90g4g/UmvW7pjV5xI/AAAAAAAASlE/iMEPAmWJ5FM/s0/Rolling_Waves_Mod-ultra-HD.jpg');    background-position: center center;    background-repeat: no-repeat;    background-size: cover;}div.innerwrap {    background: linear-gradient( rgba(15, 139, 192, 255), rgba(15, 139, 192, 0) ),;    border: transparent;}';
 document.documentElement.appendChild(styleEl);
+*/
